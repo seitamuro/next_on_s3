05f8ab931ce8de1e8b8738f66d499f4fa6679d00
seitamuro <seitamuro0210@gmail.com>
2023-01-07 23:08:05 +0900

    anotherpageへのリンクを追加

---
+++ pages/anotherpage.tsx
@@ -0,0 +1,7 @@
+export default function AnotherPage() {
+    return (
+        <>
+            This is /anotherpage
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Link from "next/link"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -116,6 +117,8 @@ export default function Home() {
               with&nbsp;Vercel.
             </p>
           </a>
+
+          <Link href="/anotherpage">go to <code>another page</code></Link>
         </div>
       </main>
     </>
